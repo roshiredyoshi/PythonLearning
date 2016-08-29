9351a0f0cbfd71b2df6e08fcba56841b872d0256
roshiredyoshi <roshiredyoshi@gmail.com>
2016-08-28 19:46:09 -0700

    Added better test cases
    Added __name__  for controlled testing purposes.

--- person.py
+++ person.py
@@ -1,3 +1,22 @@
 #File person.py (START)
 
 class Person:                                       #Start a Class
+
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+    def lastName(self):
+        return self.name.split()[-1]
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+if __name__ == '__main__':
+    #self-test code
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
+    print(bob.lastName()), sue.lastName()
+    sue.giveRaise(.10)
+    print(sue.pay)
