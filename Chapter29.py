c8da603533ba9f9d5598bd642cf7aeb80d764069
Daniel Walker <roshiredyoshi@gmail.com>
2016-09-28 12:17:25 -0700

    More chapter 29.

--- Chapter29.py
+++ Chapter29.py
@@ -3,4 +3,27 @@ class SharedData:
 
 x = SharedData()
 y = SharedData()
-print(x.spam, y.spam)
+x.spam = 88
+print(x.spam, y.spam, SharedData.spam)
+
+class MixedNames:
+        data = 'spam'
+        def __init__(self, value):
+            self.data = value
+        def display(self):
+            print(self.data, MixedNames.data)
+x = MixedNames(1)
+y = MixedNames(2)
+x.display(), y.display()
+
+class NextClass:
+    def printer(self, text):
+        self.message = text
+        print(self.message)
+x = NextClass()
+x.printer('instance call')
+x.message
+
+
+
+#Hello
