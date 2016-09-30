c27220433f6362994497917a36e244607d3047df
Daniel Walker <roshiredyoshi@gmail.com>
2016-09-30 10:41:33 -0700

    Catching GitHub up to current status

--- Chapter29.py
+++ Chapter29.py
@@ -1,29 +1,54 @@
-class SharedData:
-    spam = 42
+# class SharedData:
+#     spam = 42
+#
+# x = SharedData()
+# y = SharedData()
+# x.spam = 88
+# print(x.spam, y.spam, SharedData.spam)
+#
+# class MixedNames:
+#         data = 'spam'
+#         def __init__(self, value):
+#             self.data = value
+#         def display(self):
+#             print(self.data, MixedNames.data)
+# x = MixedNames(1)
+# y = MixedNames(2)
+# x.display(), y.display()
+#
+# class NextClass:
+#     def printer(self, text):
+#         self.message = text
+#         print(self.message)
+# x = NextClass()
+# x.printer('instance call')
+#
+# #Specializing Inherited Methods
+# class Super:
+#     def method(self):
+#         print('in Super method')
+#
+# class Sub(Super):
+#     def method(self):
+#         print('starting Sub.method')
+#         Super.method(self)
+#         print('ending Sub.method')
+#
+# x = Super()
+# x.method()
+#
+# x = Sub()
+# x.method()
+#
+class Super:
+    def delegate(self):
+        self.action()
+    def action(self):
+        raise NotImplementedError('action must be defined!')
+X = Super()
+X.delegate()
 
-x = SharedData()
-y = SharedData()
-x.spam = 88
-print(x.spam, y.spam, SharedData.spam)
+class Sub(Super): pass
 
-class MixedNames:
-        data = 'spam'
-        def __init__(self, value):
-            self.data = value
-        def display(self):
-            print(self.data, MixedNames.data)
-x = MixedNames(1)
-y = MixedNames(2)
-x.display(), y.display()
-
-class NextClass:
-    def printer(self, text):
-        self.message = text
-        print(self.message)
-x = NextClass()
-x.printer('instance call')
-x.message
-
-
-
-#Hello
+X = Sub()
+X.delegate()
---
+++ abstract_superclasses.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+
+class Super(metaclass=ABCMeta):
+    def delegate(self):
+        self.action()
+    @abstractmethod
+    def action(self):
+        pass
+
+class Sub(Super):
+    def action(self): print('spam')
+
+X = Sub()
+X.delegate()
